ee3f127c5ceecf4a044acf9bd8c61a3872df9e2d
Ol-create <oluola96@gmail.com>
2022-05-01 14:13:08 -0600

    feat: create Square component

--- src/App.js
+++ src/App.js
@@ -3,3 +3,13 @@ class Board extends React.Component {
     return <Square value={i} />;
   }
 }
+
+class Square extends React.Component{
+  render(){
+    return(
+      <button className="square" onClick={function(){
+        console.log('click');
+      }}> {this.props.value} </button>
+    )
+  }
+}
