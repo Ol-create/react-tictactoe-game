8b18239bce8fe8386710d1f896b039e8798070ba
Ol-create <oluola96@gmail.com>
2022-05-08 14:50:28 -0600

    feat: add 9 square box to board component

--- src/App.js
+++ src/App.js
@@ -10,17 +10,30 @@ class Square extends React.Component{
       this.state = { value: null,
       };
     }
-  render(){
-    return(
-      <button className="square"
-      onClick={()=> useState({value: 'X'})}
-      >
-      {this.state.value}
-      </button>
-    );
+    render() {
+      const status = 'Next player: X';
+
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
   }
-}
 
 
-
-}
